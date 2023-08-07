d559a9e0823a480523a83983b9d12e8e22c4e5b8
origuri <dlcksrb7@naver.com>
2023-08-07 21:13:00 +0900

    2023.8.7 : Spring boot + react 연습

---
+++ src/main/java/com/example/book/entity/Book.java
@@ -0,0 +1,22 @@
+package com.example.book.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Book {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // 해당 데이터베이스 번호증가 전략을 따라가겠다.
+    private Long id;
+
+    private String title;
+    private String author;
+}
